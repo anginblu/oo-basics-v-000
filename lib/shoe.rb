b5f02092db282be0580128541ef72b580c3379aa
Jacqueline Lee <anginblu@gmail.com>
2017-11-23 07:58:36 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,6 @@
 class Shoe
 
-  attr_accessor :color :size :material :condition
+  attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
   def initialize(brand)
@@ -10,4 +10,5 @@ class Shoe
     puts "the shoe has been repaird"
     @condition = "new"
   end
+
 end
